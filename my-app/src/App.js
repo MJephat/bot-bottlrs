57b5ee60c90122f913ec704c9ad6a8ea8a1564d4
MJephat <mukundijephat22@gmail.com>
2023-04-29 05:01:25 +0300

    created delete button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import BotCollection from './components/BotCollection';
+import YourBotArmy from './components/YourBotArmy';
 
 function App() {
   return (
     <div className="App">
+      <YourBotArmy />
       <BotCollection />
    
     </div>
--- my-app/src/components/BotCollection.js
+++ my-app/src/components/BotCollection.js
@@ -3,6 +3,9 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import './bot.css'
 
+
+
+
 function BotCollection(){
     //  creating State variables
     const [bots, setBots] = useState([]);
@@ -14,34 +17,53 @@ function BotCollection(){
       .then((bots) => setBots(bots));
 
   }, []);
+
+  // function for Delete from the API by id
+  function handleDelete(id){
+    alert(id)
+    fetch(`http://localhost:8001/bots/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        if (res.ok) {
+          // console.log("Are you sure you want to delete this bot");
+          setBots(bots.filter((remove) => remove.id !== id));
+        } else {
+          throw new Error("Event couldn't be completed");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
     return(
         <div>
         <div className='item-container'>
             {bots.map((bot) =>(
+            <button ocClick>
                     <div key={bot.id}>
-                    <div className='card'>
-                    
-                        <h3>Name: {bot.name}</h3>
-                        <img src ={bot.avatar_url} alt=''/>
-                        <p>Health: {bot.health}</p>
-                        <p>Image: {bot.damage}</p>
-                        <p>Armor: {bot.armor}</p>
-                        <p>Class: {bot.bot_class}</p>
-                        {/* <p>Catchphrase: {bot.catchphrase}</p> */}
-                        <p>D.O.C: {bot.created_at}</p>
-                        <p>D.O.U: {bot.updated_at}</p>
+                        <div className='card'>
                         
-                        
-                    </div> 
-                      
-                        
-
+                            <h3>Name: {bot.name}</h3>
+                            <img src ={bot.avatar_url} alt=''/>
+                            <p>Health: {bot.health} {"    "}
+                            Damage: {bot.damage} {"    "}
+                            Armor: {bot.armor}</p>
+                            <p>Class: {bot.bot_class}</p>
+                            {/* <p>Catchphrase: {bot.catchphrase}</p> */}
+                            <p>D.O.C: {bot.created_at}</p>
+                            <p>D.O.U: {bot.updated_at}</p>
+                            <button id='delete' onClick={()=> handleDelete(bot.id)}>X</button>
+                         </div> 
                     </div>
+              
+                
+            </button>    
                 )
             )
 
             }
-    
+
         </div>
         </div>
     )
